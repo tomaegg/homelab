aac1e274e0796a0b18129f1def0081855819fd61
scarlet <phantom190514@gmail.com>
2025-07-24 03:20:54 +0800

    feat: update config

--- mihomo.init.sh
+++ mihomo.init.sh
@@ -1,22 +1,27 @@
 #!/bin/sh /etc/rc.common
 
+USE_PROCD=1 # 声明使用 procd 管理服务
 START=99
 STOP=15
 PROG_PATH=/root/mihomo/mihomo
 CONF_PATH=/root/mihomo/config.yaml
 
-start() {
-  echo "Starting mihomo..."
-  ($PROG_PATH -f $CONF_PATH) &
+start_service() { # 必须使用 start_service 而非 start
+  procd_open_instance
+  procd_set_param command "$PROG_PATH" -f "$CONF_PATH"
+  procd_set_param respawn  # 进程崩溃后自动重启
+  procd_set_param stdout 1 # 重定向标准输出到系统日志
+  procd_set_param stderr 1 # 重定向错误输出到系统日志
+  procd_close_instance
 }
 
-stop() {
+stop_service() { # 必须使用 stop_service 而非 stop
   echo "Stopping mihomo..."
   kill $(ps | grep "$PROG_PATH" | grep -v 'grep' | awk '{print $1}') || true
 }
 
 restart() {
-  stop
-  start
+  stop_service
+  start_service
   echo "Restarted mihomo"
 }
