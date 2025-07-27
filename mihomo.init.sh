41e9781e60bc01b84a957e7b5fe25a30bef1fb8e
scarlet <phantom190514@gmail.com>
2025-07-27 17:41:08 +0800

    chore: update mihomo.init

--- mihomo.init.sh
+++ mihomo.init.sh
@@ -4,11 +4,11 @@ USE_PROCD=1 # 声明使用 procd 管理服务
 START=99
 STOP=15
 PROG_PATH=/root/mihomo/mihomo
-CONF_PATH=/root/mihomo/config.yaml
+CONF_PATH=/root/mihomo/
 
 start_service() { # 必须使用 start_service 而非 start
   procd_open_instance
-  procd_set_param command "$PROG_PATH" -f "$CONF_PATH"
+  procd_set_param command "$PROG_PATH" -d "$CONF_PATH"
   procd_set_param respawn  # 进程崩溃后自动重启
   procd_set_param stdout 1 # 重定向标准输出到系统日志
   procd_set_param stderr 1 # 重定向错误输出到系统日志
